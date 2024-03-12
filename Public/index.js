00a3174335c173a50ad5ea7b7a91990fcf558b21
Lud0vici <zenglouis9@gmail.com>
2024-03-12 19:41:03 -0400

    Update index.js

--- Public/index.js
+++ Public/index.js
@@ -1,6 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import LoginPage from "./src/LoginPage"; //importing login page component from react?
-
-ReactDOM.render(<LoginPage />, document.getElementById("root"));
 
