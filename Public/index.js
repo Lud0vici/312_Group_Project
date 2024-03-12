a4f3c5b1a174f3e6f3c519dbd6fa1ed977f4213b
OwJuice <76753394+OwJuice@users.noreply.github.com>
2024-03-12 19:07:05 -0400

    Update index.js

--- Public/index.js
+++ Public/index.js
@@ -3,3 +3,5 @@ import ReactDOM from "react-dom";
 import LoginPage from "./src/LoginPage"; //importing login page component from react?
 
 ReactDOM.render(<LoginPage />, document.getElementById("root"));
+
+//hello
