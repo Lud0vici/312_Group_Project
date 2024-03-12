92207cec9512044774000e41ff6b4810125596bd
OwJuice <76753394+OwJuice@users.noreply.github.com>
2024-03-12 19:05:05 -0400

    Took index.js from github

--- Public/index.js
+++ Public/index.js
@@ -1,1 +1,5 @@
-function testing
+import React from "react";
+import ReactDOM from "react-dom";
+import LoginPage from "./src/LoginPage"; //importing login page component from react?
+
+ReactDOM.render(<LoginPage />, document.getElementById("root"));
