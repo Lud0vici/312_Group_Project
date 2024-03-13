efb305a5986038a0e31e389b5463af18604329e7
Lud0vici <zenglouis9@gmail.com>
2024-03-12 20:34:07 -0400

    changed Public file to public

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def serve_index():
-    return send_from_directory('Public', 'index.html')
+    return send_from_directory('public', 'index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
