09abb308635a4206fc5e9534a4a5b912fff3d574
Wesley Meah <wesleymeah@gmail.com>
2024-03-12 20:21:40 -0400

    send from directory implementation

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import socketserver
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
 
+def serve_index():
+    return send_from_directory('Public', 'index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
