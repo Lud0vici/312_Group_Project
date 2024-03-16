cfe24efee2556d702d82543fb6cbabaa36260505
Lud0vici <zenglouis9@gmail.com>
2024-03-15 20:35:34 -0400

    Update server.py

--- server.py
+++ server.py
@@ -5,8 +5,7 @@ from util import database_handler
 from util import auth
 from util.database_handler import user_collection
 import hashlib
-import datetime
-
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__, template_folder="src")
@@ -55,22 +54,18 @@ def serve_registration():
     last_name = user_credentials[1]
     email = user_credentials[2]
     username = user_credentials[3]
-    #print(username)
     password = user_credentials[4]
     confirmedPassword = user_credentials[5]
-    # print("before auth validate")
+
     validPassword = auth.validate_password(password)
-    # print("checking database")
     user_data = user_collection.find_one({"username": username})   #error
     user_email = user_collection.find_one({"email": email})
-    if user_data is not None:       #needs more testing when login and homepage are created
-        # flash("Username already taken!")
+
+    if user_data is not None:
         response = make_response("Username already taken!")
         add_no_sniff(response)
         response.status_code = 404
-        #return redirect(url_for('serve_registration_page'))
         return response
-        # return render_template("RegistrationPage.html", error_message="Username already taken!")
     elif auth.validate_username(username) is False:
         response = make_response("Username is invalid!")
         add_no_sniff(response)
@@ -80,32 +75,23 @@ def serve_registration():
         response = make_response("Email is associated with an account!")
         add_no_sniff(response)
         response.status_code = 404
-        # return redirect(url_for('serve_registration_page'))
         return response
     elif validPassword != True:
-        # flash("Password does not meet requirements")
         response = make_response("Password does not meet requirements!")
         add_no_sniff(response)
         response.status_code = 404
-        #return redirect(url_for('serve_registration_page'))
         return response
-        # return render_template("RegistrationPage.html", error_message="Password does not meet requirements")
     elif password != confirmedPassword:
-        # flash("Passwords do not match")
         response = make_response("Passwords do not match!")
         add_no_sniff(response)
         response.status_code = 404
-        #return redirect(url_for('serve_registration_page'))
         return response
-        # return render_template("RegistrationPage.html", error_message="Passwords do not match")
     else:
         salt, hashed_password = database_handler.salt_and_hash_password(password)
         database_handler.insert_user(first_name, last_name, email, username, salt, hashed_password)
         print(database_handler.user_collection.find_one({"username": username})["username"])
-        #return redirect("/", code=302)
         return redirect(url_for("serve_login_page"))
 
-        #return redirect(url_for("registration_form")) #need to adjust regist.... 
 
 @app.route("/login", methods=["POST"])
 def serve_login():
@@ -119,31 +105,31 @@ def serve_login():
     user_data = database_handler.user_collection.find_one({"username": username})
 
 
-    if user_data is None:   #if user not found in db, flash "User not found"
-        #flash("User not found")
+    if user_data is None:
         response = make_response("User not found")
         add_no_sniff(response)
         response.status_code = 404
         return response
-    else:   #if user is found, we now have to check the password
+    else:
         salt = user_data["salt"]
         salted_password = password + salt
         curr_user_password = hashlib.sha256(salted_password.encode()).hexdigest()
 
         if curr_user_password == user_data["password"]:     #if password matches the one found in the user_collection db
             session["username"] = username
-            print(session)
             token = secrets.token_urlsafe(32)
             hashed_token = hashlib.sha256(token.encode()).hexdigest()
             database_handler.user_collection.update_one({"username": username}, {"$set": {"auth_token": hashed_token}})
             response = redirect(url_for('serve_homepage'))      #make a response with an empty body
-            expire_date = datetime.datetime.now()
-            expire_date = expire_date + datetime.timedelta(minutes=60)
+            expire_date = datetime.now()
+            expire_date = expire_date + timedelta(minutes=60)
             response.set_cookie("authentication-token", token, httponly=True, expires=expire_date, max_age=3600)     #set auth-token cookie
             session['username'] = username
+            session.permanent = True
+            app.permanent_session_lifetime = timedelta(minutes=60)
+            session.modified = True
             return response
         else:
-            #flash("Incorrect password")
             response = make_response("Incorrect password")
             add_no_sniff(response)
             response.status_code = 404
@@ -151,10 +137,17 @@ def serve_login():
 
 @app.route("/homepage")
 def serve_homepage():
-    response = send_from_directory("src", "HomePage.html")
-    add_no_sniff(response)
     username = session.get("username")
-    #response.set_cookie("username", username)       #dont need, only testing username
+
+    with open("src/HomePage.html", "rb") as file:
+        file_contents = file.read()
+
+    file_contents = file_contents.replace(b"{{user}}", username.encode())
+
+    response = make_response(file_contents)
+    add_no_sniff(response)
+
+
     return response
 
 @app.route("/logout", methods=["POST"])
