966e90cea8df718948ed9de1eb7ede28d537ca0b
Lud0vici <zenglouis9@gmail.com>
2024-04-14 14:39:03 -0400

    latest edit

--- public/homepage_functions.js
+++ public/homepage_functions.js
@@ -8,29 +8,29 @@ function toggleLike(index) {
     renderPosts();
 }
 
-document.getElementById('createPostBtn').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'block';
-});
-
-document.getElementById('cancelPostBtn').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'none';
-});
-
-document.getElementById('submitPostBtn').addEventListener('click', function() {
-    const content = document.getElementById('postContent').value.trim();
-    if (content !== '') {
-        const post = {
-            username: 'User', // You can replace 'User' with actual username
-            content: content,
-            likes: 0,
-            liked: false
-        };
-        posts.push(post);
-        renderPosts();
-        document.getElementById('popup').style.display = 'none';
-        document.getElementById('postContent').value = '';
-    }
-});
+//document.getElementById('createPostBtn').addEventListener('click', function() {
+//    document.getElementById('popup').style.display = 'block';
+//});
+//
+//document.getElementById('cancelPostBtn').addEventListener('click', function() {
+//    document.getElementById('popup').style.display = 'none';
+//});
+//
+//document.getElementById('submitPostBtn').addEventListener('click', function() {
+//    const content = document.getElementById('postContent').value.trim();
+//    if (content !== '') {
+//        const post = {
+//            username: 'User', // You can replace 'User' with actual username
+//            content: content,
+//            likes: 0,
+//            liked: false
+//        };
+//        posts.push(post);
+//        renderPosts();
+//        document.getElementById('popup').style.display = 'none';
+//        document.getElementById('postContent').value = '';
+//    }
+//});
 
 function chatMessageHTML(messageJSON) {
     console.log(messageJSON)
